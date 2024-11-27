8af2d12ace8e048fc582c800a1dab5a5fb793b13
Sonal Singh <sonal1201kumar@gmail.com>
2024-11-27 22:02:06 +0530

    basic DSA practice

---
+++ Dp_Problems/Grid_DP/greekTraining.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+int f(int day, int last, vector<vector<int>> &points, vector<vector<int>> &dp)
+{
+
+    if (day == 0)
+    {
+        int maxi = 0;
+        for (int task = 0; task < 3; task++)
+        {
+            if (task != last)
+                maxi = max(maxi, points[0][task]);
+        }
+        return maxi;
+    }
+    if (dp[day][last] != -1)
+        return dp[day][last];
+    int maxi = 0;
+    for (int i = 0; i < 3; i++)
+    {
+        if (i != last)
+        {
+            int point = points[day][i] + f(day - 1, i, points, dp);
+            maxi = max(point, maxi);
+        }
+    }
+    return dp[day][last] = maxi;
+}
+
+int ninjaTraining(int n, vector<vector<int>> &points)
+{
+    vector<vector<int>> dp(n, vector<int>(4, -1));
+    return f(n - 1, 3, points, dp);
+}
